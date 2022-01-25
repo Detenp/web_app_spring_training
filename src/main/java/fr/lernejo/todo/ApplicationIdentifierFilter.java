aa9f16f945e7ff383495271cae5f5533e87832f7
lounes.bounouar <lounes.bounouar@epita.fr>
2022-01-25 12:39:17 +0100

    Part 4

---
+++ src/main/java/fr/lernejo/todo/ApplicationIdentifierFilter.java
@@ -0,0 +1,20 @@
+package fr.lernejo.todo;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class ApplicationIdentifierFilter implements Filter {
+    private final String uuid = UUID.randomUUID() + "";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Instance-Id", uuid);
+        filterChain.doFilter(servletRequest, response);
+    }
+}
